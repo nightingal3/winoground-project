ecbcb24d228ee931b6fe0c545a8f29aa51474920
Meng Yan Liu <emmy.liu.1998@gmail.com>
2022-11-20 19:54:00 -0500

    add basic options for negatives

---
+++ src/VLMOrder/perturbtest.py
@@ -0,0 +1,25 @@
+from perturbtextorder import *
+from perturbimage import *
+from PIL import Image
+
+
+
+
+text1 = "remarkable scene with a blue ball behind a green chair"
+text2 = "The old man is slowly eating the sandwich and the pretty woman is calmly watching the television"
+print("Evaluation Perturbations")
+print("Original Text 1:",text1)
+print("Shuffling Nouns and Adjectives:",nounadjshuf(text1))
+print("Shuffling everything but Nouns and Adjectives:",nonnounadjshuf(text1))
+print("Shuffling Trigrams:",trigramshuf(text1))
+print("Shuffling Words in Trigrams:",wordtrigramshuf(text1))
+print("Finetuning Perturbations")
+print("Original Text 2:",text2)
+print("Shuffling Nouns:",nounshuf(text2))
+print("Shuffling Adjectives:",adjshuf(text2))
+print("Shuffling Adverbs:",advshuf(text2))
+print("Shuffling Noun Chunks:",nounchunkshuf(text2))
+
+im = Image.open("87_1.png")
+perturbim = RandomPatch(im,16)
+perturbim.show()
--- src/main.py
+++ src/main.py
@@ -5,35 +5,46 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 from torchvision import transforms
 import numpy as np
+import argparse
 
 from data.mscoco import COCO, COCODataset
-
+from VLMOrder.perturbtextorder import nounadjshuf
+from VLMOrder.perturbimage import RandomPatch
 import pdb
 
-COCO_PATH="/projects/tir1/corpora/COCO/"
-BATCH_SIZE=32
-
 if __name__ == "__main__":
-    default_transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='ViT-B/32')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--use_distractors', action='store_true')
+    parser.add_argument('--coco_path', type=str, default="/projects/tir1/corpora/COCO/")
+    parser.add_argument('--caption_year', type=str, default="2017")
+    args = parser.parse_args()
 
-    dataset = COCODataset(root=COCO_PATH, caption_year="2017", image_transforms=None)
-    train_dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+    dataset = COCODataset(root=args.coco_path, caption_year=args.caption_year, image_transforms=None)
+    train_dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
-    model, preprocess = clip.load("ViT-B/32")
+    model, preprocess = clip.load(args.model)
     model = model.float().cuda()
 
-    loss = nn.CrossEntropyLoss()
+    loss_image = nn.CrossEntropyLoss()
+    loss_text = nn.CrossEntropyLoss()
     optimizer = optim.AdamW(model.parameters()) 
     
     for i in range(1000):
         optimizer.zero_grad()
-        inds = np.random.choice(len(dataset), BATCH_SIZE)
+        inds = np.random.choice(len(dataset), args.batch_size)
         train_data = [dataset[i] for i in inds]
         #train_images = [train_data['image_0'] + train_data['image_1']]
         train_images = [train_data[i]["image"] for i in range(len(train_data))]
         train_text = [train_data[i]["caption"] for i in range(len(train_data))]
+
+        if args.use_distractors: # TODO: more sophisticated distractors
+            distractor_text = [nounadjshuf(train_data[i]["caption"]) for i in range(len(train_data))]
+            train_text = train_text + distractor_text
+
+            distractor_images = [RandomPatch(train_data[i]["image"], 4) for i in range(len(train_data))]
+            train_images = train_images + distractor_images
 
         train_images = [preprocess(img) for img in train_images]
         train_images = torch.stack(train_images, dim=0).cuda()
@@ -41,9 +52,13 @@ if __name__ == "__main__":
         
         logits_per_image, logits_per_text = model(train_images, train_text)
         
-        loss_i = loss(logits_per_image, torch.arange(BATCH_SIZE).cuda())
-        loss_t = loss(logits_per_text, torch.arange(BATCH_SIZE).cuda())
-        
+        if args.use_distractors: # distractors have no corresponding label
+            logits_per_image = logits_per_image[:args.batch_size, :args.batch_size]
+            logits_per_text = logits_per_text[:args.batch_size, :args.batch_size]
+
+        loss_i = loss_image(logits_per_image, torch.arange(args.batch_size).cuda())
+        loss_t = loss_text(logits_per_text, torch.arange(args.batch_size).cuda())
+
         total_loss = (loss_i + loss_t) / 2
         total_loss.backward()
         print(total_loss)
